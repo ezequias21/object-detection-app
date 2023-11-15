9bd8e49c3d7c5c9a261910a340e1fa0f186ae732
Ezequias <ezequias.antunes@sysvale.com>
2023-11-15 11:57:20 -0300

    feat: cria arquivos de rotas de autenticação

---
+++ src/routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { login, logout } = require("../../auth/auth")
+
+//Routes
+router.get('/', (req, res) => res.render('home'))
+router.get('/login', (req, res) => res.render('login'))
+router.get('/logout', logout)
+router.post('/login', login)
+
+module.exports = router
