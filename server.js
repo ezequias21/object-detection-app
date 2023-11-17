23580a68b667c5726b7615c2637d9769a67697e7
Ezequias <ezequias.antunes@sysvale.com>
2023-11-17 11:43:29 -0300

    feat: emite eventos como volatile

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const listenerFrame = function(roomId){
 
 		if(roomId == Room.code) {
 			sockets.join(roomId)
-			sockets.to(roomId).broadcast.emit('new-frame', data);
+			sockets.to(roomId).volatile.emit('new-frame', data);
 		}
 	});
 }
