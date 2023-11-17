9a2c5998eb7f6cf6343ea9330f1229474b6dc955
Ezequias <ezequias.antunes@sysvale.com>
2023-11-16 21:07:38 -0300

    fix: refaz lógica de verificação de inatividade

--- public/script.js
+++ public/script.js
@@ -2,32 +2,41 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const img = document.querySelector('.card-image__frame');
 var timeout = 0;
+var lastTimeFrameUpdated = new Date();
+
+const inactivity = (timePassed, now) =>{
+    var diff = Math.abs(now.getTime() - timePassed.getTime());
+    var diffSeconds = Math.floor(diff / (1000));
+
+    return diffSeconds > 1
+}
+
+const signalStatus = () => {
+    if(inactivity(lastTimeFrameUpdated, new Date())) {
+        document.querySelector('.signal-status').innerHTML =
+        `<div>Sinal da transmissão inativo</div><div class="round red"></div></a>`
+        img.src = '/background.png'
+    }else {
+        document.querySelector('.signal-status').innerHTML =
+        `<div>Sinal da transmissão ativo</div><div class="round light-green"></div></a>`
+    }
+}
 
 if(code) {
-  socket.emit('start', code)
+    socket.emit('start', code)
 
-  socket.on('new-frame', data => {
-    const imagemBase64 = btoa(String.fromCharCode(...new Uint8Array(data)));
-    img.src = 'data:image/jpg;base64,' + imagemBase64;
+    socket.on('new-frame', data => {
+        const imagemBase64 = btoa(String.fromCharCode(...new Uint8Array(data)));
+        img.src = 'data:image/jpg;base64,' + imagemBase64;
 
-    console.log('New-frame')
-  })
+        lastTimeFrameUpdated = new Date();
 
-  socket.on('signal', () => {
-
-    document.querySelector('.signal-status').innerHTML = `
-      <div>Sinal da transmissão ativo</div><div class="round light-green"></div></a>
-    `
-    clearTimeout(timeout)
-    timeout = setTimeout(() => {
-      document.querySelector('.signal-status').innerHTML = `
-        <div>Sinal da transmissão inativo</div><div class="round red"></div></a>
-      `
-      img.src = '/background.png'
-    }, 200)
+        console.log('New-frame')
   })
 }
 
+
+setInterval(signalStatus, 2000);
 
 document.querySelector('#copy-room-code').addEventListener('click', function(){
 	const roomCode = document.querySelector('#room-code').innerText
