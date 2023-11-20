d949543c9ebec94dec6598e5fbd2ade636d37d8c
Ezequias <ezequias.antunes@sysvale.com>
2023-11-20 13:04:05 -0300

    feat: adiciona atributo expiração no model de sala

--- src/models/room.js
+++ src/models/room.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const roomSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     roomCode: { type: String, required: true },
+    expiredAt: {type: Date, required: true}
 });
 
 const Room = mongoose.model('Room', roomSchema);
